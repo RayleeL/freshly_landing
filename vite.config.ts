f2204423c5a7c48e377026706cdbf294ce87cc29
Амира Асанова <amiraasanova443@gmail.com>
2026-03-31 15:34:12 +0600

    fix vite file

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: 'https://github.com/RayleeL/freshly_landing/',
+    base: '/freshly---natural-fruit-smoothies/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
